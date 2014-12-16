a32490c368e77788a5c487b918aac7b38b94f068
h_dung <h_dung@brycen.com.vn>
2014-12-16 09:33:16 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = 'c3681f1c472834d21119207cf5d611d89de92cbb4312e445305009c354738fe7d5b6341271a147c47952f5f8c4ffeb209ca1ec43d2b14ff055b7c15cc476becf'
---
+++ spec/steps/view_list_steps.rb
@@ -0,0 +1,7 @@
+#encoding: utf-8
+puts 'hello'
+step "admin đăng nhập vào trang quản lý" do 
+	visit '/admin'
+end
+
+
